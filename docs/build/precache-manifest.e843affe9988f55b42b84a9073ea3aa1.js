7bda99b2d870ba335ebf9c5a5db3917cdecb7fd2
Maria Garcia de Lomana <mariagarciadelomana@gmail.com>
2020-01-23 11:08:42 +0100

    add docs folder to be published in GitHub Pages

---
+++ docs/build/precache-manifest.e843affe9988f55b42b84a9073ea3aa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c017638dca037b7017286ea4e32c5b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26d784dc0db2de3374e2",
+    "url": "./static/css/main.4419caf8.chunk.css"
+  },
+  {
+    "revision": "b4e7c120585bc46a49e4",
+    "url": "./static/js/2.afc8be11.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.afc8be11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26d784dc0db2de3374e2",
+    "url": "./static/js/main.e4b126b0.chunk.js"
+  },
+  {
+    "revision": "28bbc1b77944e55f5cc7",
+    "url": "./static/js/runtime-main.d753a5ee.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
